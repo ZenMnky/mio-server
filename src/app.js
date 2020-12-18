5e4b4bafd4ff9d3b3187a0885fe834b98d58d2a2
Justin Hager <coachjustinhager@gmail.com>
2020-12-18 12:04:51 -0800

    more tests

--- src/app.js
+++ src/app.js
@@ -9,13 +9,25 @@ const profilesRouter = require('./profiles/profiles-router');
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
 const app = express();
+
 app.use(morgan(morganOption));
+  app.use((req, res, next) => {
+    console.log('req body after morgan')
+    console.table(req.body)
+    next()
+  })
 app.use(helmet());
+  app.use((req, res, next) => {
+    console.log('req body after helmet')
+    console.table(req.body)
+    next()
+  })
+
 app.use(cors());
-app.use((req, res, next) => {
-  console.log('req body')
-  console.table(req.body)
-  next()
+  app.use((req, res, next) => {
+    console.log('req body after cors')
+    console.table(req.body)
+    next()
 })
 app.use(express.json());
 
