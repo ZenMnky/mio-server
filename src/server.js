dd538e5a1241cc55825fa872a21bc7ae74938edd
Justin Hager <coachjustinhager@gmail.com>
2021-05-18 12:11:46 -0700

    update configuration for node 14 and heroku

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const { PORT, DATABASE_URL } = require("./config");
 const knex = require('knex');
 
 const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production";
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 
 const db = knex({
   client: 'pg',
